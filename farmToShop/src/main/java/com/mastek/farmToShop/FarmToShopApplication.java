bae02aea3db2d7f49377b85c5dee44815ce2bcd5
Joseph61288 <Joseph61288@GB-LD1LPW10G019.mshome.net>
2020-09-02 09:37:10 +0100

    Initial commit with changes to the application properties to link with mySQL

---
+++ farmToShop/src/main/java/com/mastek/farmToShop/FarmToShopApplication.java
@@ -0,0 +1,13 @@
+package com.mastek.farmToShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmToShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmToShopApplication.class, args);
+	}
+
+}
